f02ecd10b73e36bb42ce54d76d45bc63fb5e99dd
Dmitry Dukhovskoy <dmitry.dukhovskoy@noaa.gov>
2025-10-10 14:19:30 -0400

    merging gfdel_public to master-hera

--- request_nodes.sh
+++ request_nodes.sh
@@ -3,4 +3,6 @@
 # to run python or debugging
 # max hours - 12 but check:
 # sacctmgr show qos format=Name,MaxWall 
-salloc --x11 -q batch -t 10:00:00 --ntasks=1 --clusters=c5 -A cefi
+#salloc --x11 -q batch -t 10:00:00 --ntasks=1 --clusters=c5 -A cefi
+# On gaea:
+salloc -M c5 -t 10:00:00
